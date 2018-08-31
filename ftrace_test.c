f23a3d902407fba4123a9a90029682f6acc4de19
Chris Misa <chris@chrismisa.com>
2018-08-31 11:14:15 -0700

    added parsing

--- ftrace_test.c
+++ ftrace_test.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define READ_BUF_SIZE 256
+#define NAME_BUF_SIZE 64
 
 void usage()
 {
@@ -23,11 +27,88 @@ void echo_to(const char *file, const char *data)
   fclose(fp);
 }
 
+
+// Ugly parse of text string read from pipe
+// In the future this will be replaced by binary reads
+
+enum event_types {
+  EVENT_TYPE_NONE,
+  EVENT_TYPE_ENTER_SENDTO,
+  EVENT_TYPE_EXIT_SENDTO,
+  EVENT_TYPE_ENTER_RECVMSG,
+  EVENT_TYPE_EXIT_RECVMSG
+};
+
+struct trace_event {
+  struct timeval   ts;
+  enum event_types type;
+};
+
+struct trace_event *get_trace_event(char *str)
+{
+  struct trace_event *evt;
+  int dot_count = 0;
+  char *end = NULL;
+  int name_len = 0;
+  char name_buf[NAME_BUF_SIZE];
+  
+  evt = (struct trace_event *)malloc(sizeof(struct trace_event));
+  evt->ts.tv_sec = 0;
+  evt->ts.tv_usec = 0; 
+  evt->type = EVENT_TYPE_NONE;
+  
+  // Look for four dot separator
+  while (dot_count < 4 && *str != '\0') {
+    if (*str != '.') {
+      dot_count = 0;
+    } else {
+      dot_count++;
+    }
+    str++;
+  } 
+
+  fprintf(stderr, "Found dots, ");
+
+  // Get seconds and micro seconds
+  evt->ts.tv_sec = strtol(str, &end, 10);
+  str = end + 1; // skip the decimal point
+  evt->ts.tv_usec = strtol(str, &end, 10);
+  str = end + 6; // skip the colon, space, and 'sys_' prefix
+
+  fprintf(stderr, "timestamp: %lu.%06lu, ", evt->ts.tv_sec,
+                                              evt->ts.tv_usec);
+
+  // get the event type string
+  end = str;
+  while (*end != ' ' && *end != '(' && *end != '\0') {
+    end++;
+  }
+  
+  name_len = end - str;
+  // silently truncate if longer than buffer
+  if (name_len > NAME_BUF_SIZE) {
+    name_len = NAME_BUF_SIZE;
+  }
+  memcpy(name_buf, str, name_len);   
+  name_buf[name_len] = '\0';
+
+  fprintf(stderr, "event name: %s\n\n", name_buf);
+
+  return evt;
+}
+
+void free_trace_event(struct trace_event *evt)
+{
+  free(evt);
+}
+
+
+
 int main(int argc, char *argv[])
 {
   const char *tracefp = "/sys/kernel/debug/tracing";
   FILE *trace_pipe;
-  char buf[100];
+  char buf[READ_BUF_SIZE];
   int pid;
 
   if (argc != 2) {
@@ -50,8 +131,9 @@ int main(int argc, char *argv[])
   }
   
   while (1) {
-    fgets(buf, 100, trace_pipe);
-    printf("Read: %s\n", buf);
+    fgets(buf, READ_BUF_SIZE, trace_pipe);
+    fprintf(stderr, "Read: %s\n", buf);
+    get_trace_event(buf);
   }
 
   echo_to("tracing_on", "0");
