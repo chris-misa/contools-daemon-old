61e1d0d5620ce27e4ff5a35abacd391be527023f
Chris Misa <chris@chrismisa.com>
2018-09-03 15:49:37 -0700

    Added rough experiment scipt

---
+++ run1.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+# Test of iface_diff latency daemon
+#
+B="----------------"
+
+
+TARGET_IPV4="172.217.14.238"
+
+PING_ARGS="-i 1.0 -s 56 -c 10"
+
+NATIVE_PING_CMD="$HOME/Dep/iputils/ping"
+CONTAINER_PING_CMD="/iputils/ping"
+
+PING_CONTAINER_IMAGE="chrismisa/contools:ping"
+PING_CONTAINER_NAME="ping-container"
+
+PAUSE_CMD="sleep 5"
+
+IFACE_1="docker0"
+IFACE_2="wlp2s0"
+LATENCY_CMD="$(pwd)/iface_diff"
+
+DATE_TAG=`date +%Y%m%d%H%M%S`
+
+mkdir $DATE_TAG
+cd $DATE_TAG
+
+# Start ping container as service
+docker run --rm -itd \
+  --name=$PING_CONTAINER_NAME \
+  --entrypoint=/bin/bash \
+  $PING_CONTAINER_IMAGE
+echo $B Started $PING_CONTAINER_NAME $B
+
+$PAUSE_CMD
+
+# Native pings for control
+echo $B Native control $B
+$NATIVE_PING_CMD $PING_ARGS $TARGET_IPV4 \
+  > native_${TARGET_IPV4}.ping
+
+$PAUSE_CMD
+
+# Container pings for control
+echo $B Container control $B
+docker exec $PING_CONTAINER_NAME \
+  $CONTAINER_PING_CMD $PING_ARGS $TARGET_IPV4 \
+  > container_control_${TARGET_IPV4}.ping
+
+$PAUSE_CMD
+
+# Container pings with monitoring
+echo $B Container monitored $B
+# Run monitor in background
+$LATENCY_CMD $IFACE_1 $IFACE_2 \
+  > container_monitored_${TARGET_IPV4}.lat &
+MONITOR_PID=$!
+echo "  monitor running with pid: ${MONITOR_PID}"
+
+$PAUSE_CMD
+
+echo "  container pinging. . ."
+docker exec $PING_CONTAINER_NAME \
+  $CONTAINER_PING_CMD $PING_ARGS $TARGET_IPV4 \
+  > container_monitored_${TARGET_IPV4}.ping
+
+$PAUSE_CMD
+kill -INT $MONITOR_PID
+echo "  killed monitor"
+
+$PAUSE_CMD
+echo Done.
